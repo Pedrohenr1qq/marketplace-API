10dd7e2727f4826dd402203ad2af82344bae7b2c
Pedro Henrique <pedrohenr1qqstudios@gmail.com>
2024-11-29 23:45:17 -0300

    chore: add some changes

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,21 @@
+// Imports
 import express, {json} from "express";
 import cors from "cors";
 import ConnectToMongoDB from "database";
+import router from "routes";
 
+// Configure server
 const app = express();
 app.use(json());
 app.use(cors());
 
+// Start DB
 ConnectToMongoDB.execute();
 
+// Start routes
+app.use(router);
+
+// Start server
 const port = process.env.PORT || 5000;
 app.listen(port, ()=>{
   console.log(`Server running at port: ${port}`);
