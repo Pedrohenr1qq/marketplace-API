b56da3bc635fe29da810533b3a19166205f49d6d
Pedro Henrique <pedrohenr1qqstudios@gmail.com>
2025-01-17 12:58:55 -0300

    Refactor: Custom Errors Handling IV (Auth and SchemaValidation)

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -3,6 +3,7 @@ import "dotenv/config"
 import jwt, { JwtPayload } from "jsonwebtoken"
 import { container } from "tsyringe";
 import { FindByIdService } from "modules/Users/useCases/findById/findByIdService";
+import { UnauthorizedError } from "helpers/errors/apiError";
 
 interface IToken extends JwtPayload{
   id: string; 
@@ -11,27 +12,27 @@ interface IToken extends JwtPayload{
 class AuthMiddleware{
   async execute(req: Request, res: Response, next: NextFunction){
     const {authorization} = req.headers;
-    if(!authorization) throw new Error("invalid token");
+    if(!authorization) throw new UnauthorizedError("invalid token");
     
     const secret = process.env.SECRET_JWT as string;
 
     const parts = authorization?.split(" ");
-    if(!parts.length) res.status(401).send("Invalid token")
-    if(parts.length != 2) res.status(401).send("Invalid token")
+    if(!parts.length) throw new UnauthorizedError("invalid token");
+    if(parts.length != 2) throw new UnauthorizedError("invalid token");
     
     const [schema, token] = parts;
-    if (!/^Bearer$/i.test(schema)) res.status(401).send("Invalid token");
+    if (!/^Bearer$/i.test(schema)) throw new UnauthorizedError("invalid token");
 
     jwt.verify(token, secret, async(err, decoded) => {
-      if(err) res.status(401).send("Invalid token");
-      if(!decoded) res.status(401).send("Invalid token");
+      if(err) throw new UnauthorizedError("invalid token");
+      if(!decoded) throw new UnauthorizedError("invalid token");
     
       const {id} = decoded as IToken;
 
       try {
         const findByIdService = container.resolve(FindByIdService);
         const user = await findByIdService.execute(id);
-        if(!user) res.status(401).send("Invalid token");
+        if(!user) throw new UnauthorizedError("invalid token");
         
         res.locals.user = user;
 
@@ -40,11 +41,6 @@ class AuthMiddleware{
       } catch (error: any) {
         return res.status(500).send(error.message);
       }
-
-
     });
-
   }
-}
-
-export default new AuthMiddleware();
+} export default new AuthMiddleware();
--- src/middlewares/schemaValidationMiddleware.ts
+++ src/middlewares/schemaValidationMiddleware.ts
@@ -1,4 +1,5 @@
 import {Request, Response, NextFunction } from "express";
+import { ConflitError } from "helpers/errors/apiError";
 import { UserSchemaJoi } from "modules/Users/schemas/joi/UserSchemaJoi";
 
 class ValidateSchema{
@@ -7,7 +8,7 @@ class ValidateSchema{
       const {error} = schema.validate(req.body, {abortEarly: false});
       if(error) {
         const errors = error?.details.map((detail: any) => detail.message);
-        res.status(409).send(errors);
+        throw new ConflitError(errors);
       }
       else return next();
     }
