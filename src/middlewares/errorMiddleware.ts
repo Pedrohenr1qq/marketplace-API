4b00ae65ef3140ef992b994e982ee1275ff68aa3
Pedro Henrique <pedrohenr1qqstudios@gmail.com>
2025-01-17 12:38:41 -0300

    feat: Add custom error handling

---
+++ src/middlewares/errorMiddleware.ts
@@ -0,0 +1,10 @@
+import { NextFunction, Request, Response } from "express";
+import { ApiError } from "helpers/errors/apiError";
+
+class ErrorMiddleware{
+  async execute(error: Error & ApiError, req: Request, res: Response, next: NextFunction){
+    const statusCode = error.statusCode ?? 500;
+    const message = (statusCode != 500) ? error.message : "Internal Server Error";
+    res.status(statusCode).send({message});
+  }
+} export default new ErrorMiddleware();
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 // Imports
+import "express-async-errors"
 import "reflect-metadata";
 import "./helpers/container"
 import express, {json} from "express";
 import cors from "cors";
 import ConnectToMongoDB from "database";
 import router from "routes";
+import errorMiddleware from "middlewares/errorMiddleware";
 
 // Configure server
 const app = express();
@@ -16,6 +18,7 @@ ConnectToMongoDB.execute();
 
 // Start routes
 app.use(router);
+app.use(errorMiddleware.execute);
 
 // Start server
 const port = process.env.PORT || 5000;
