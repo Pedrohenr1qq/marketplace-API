cb6079f93041783a52a1ef005bec6195d237550b
Pedro Henrique <pedrohenr1qqstudios@gmail.com>
2025-01-22 16:36:02 -0300

    Feat: Add Cart entitie and schema

---
+++ src/modules/Carts/schemas/CartSchema.ts
@@ -0,0 +1,18 @@
+import { model, Schema } from "mongoose";
+import { Cart } from "../entities/Cart";
+
+const CartShcema = new Schema<Cart>({
+  products: [
+    {
+      _id: {type: Schema.Types.ObjectId, required: true, ref: "products"},
+      quantity: {type: Number, required: true}
+    }
+  ],
+  total_price: {type: Number, required: true},
+  freight: {type: Number, required: true},
+  user_id: {type: Schema.Types.ObjectId, required: true, ref: "users"},
+  concluded: {type: Boolean, default: false},
+  created_at: {type: Date, required: true, default: Date.now()}
+});
+
+export default model<Cart>("orders", CartShcema);
